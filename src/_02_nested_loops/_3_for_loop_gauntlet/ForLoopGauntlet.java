98d95003c571b8398956c8dd46f6379eb802ec54
booksandboba001 <evie.wuornos@students.jointheleague.org>
2025-05-31 21:27:39 +0000

    finishing up ForLoopGauntlet

--- src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/ForLoopGauntlet.java
@@ -22,6 +22,39 @@ public class ForLoopGauntlet {
                 System.out.println(e+" is odd");
             }
         }
-
+        for (int f=0;f<=77;f+=7){
+            System.out.println(f);
+        }
+        int age=0;
+        for (int g=2012;g<=2025;g++){
+            age++;
+            System.out.println("In "+g+" I was "+age);
+            if(age==14&&g==2025){
+                System.out.println("It is "+g+", and I am "+age);
+            }
+        }
+        for (int h=0;h<=1;h+=2){
+            for (int i=0;i<=1;i+=2){
+                System.out.println(i);
+            }
+        } 
+        for (int j=0;j<9;j+=3){
+            for(int k=1;k<=3;k++){
+                System.out.print(" "+(k+j)+", ");
+            }
+            System.out.println();
+        }
+        for (int l=0;l<100;l+=10){
+            for(int m=1;m<=10;m++){
+                System.out.print(" "+(l+m)+", ");
+            }
+            System.out.println();
+        }
+        for (int n=0;n<6;n++){
+            for(int o=1;o<=(n+1);o++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
